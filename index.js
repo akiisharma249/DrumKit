57f9659e60dde69728c87ed6e450e544507d929b
akiisharma249 <73683898+akiisharma249@users.noreply.github.com>
2021-02-18 02:39:58 -0800

    Virtual DrumKit files

---
+++ index.js
@@ -0,0 +1,70 @@
+var hello = document.getElementsByClassName("drum");
+
+for (var i=0; i<hello.length; i++){
+
+    hello[i].addEventListener("click", function(){
+        
+        var buttonInnerHTML = this.innerHTML;
+        switchFunction(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
+    });   
+}
+
+document.addEventListener("keydown", function(key1){
+    switchFunction(key1.key);
+    buttonAnimation(key1.key);
+});
+
+function switchFunction(event){
+
+    switch(event){
+
+        case "w" :
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+        break;
+
+        case "a" :
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+
+        case "s" :
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+        break;
+
+        case "d" :
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+        break;
+
+        case "j" :
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+        break;
+
+        case "k" :
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+        break;
+
+        case "l" :
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+        break;
+
+        default :
+            console.log("Key Pressed");
+    }
+}
+
+function buttonAnimation(currentKey){
+
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    }, 100);
+}
